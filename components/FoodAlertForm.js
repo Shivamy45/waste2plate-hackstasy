8d33ac5deb90164480497d15bfd5c2445ea752ac
Shivam <shivamy476@gmail.com>
2025-04-17 13:08:55 +0530

    final

--- components/FoodAlertForm.js
+++ components/FoodAlertForm.js
@@ -6,7 +6,6 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useEffect, useRef, useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { db } from "@/firebase/config";
